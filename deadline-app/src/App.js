f09a5da7d63812e6b0f68aed2d8182570ae1e672
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-10 14:58:41 +0200

    Initial Form component with css.

--- deadline-app/src/App.js
+++ deadline-app/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './scss/Main.scss';
+// Import components
+import Form from './components/Form';
 
 function App() {
-  return <div>Moi</div>;
+  return (
+    <div className='form'>
+      <Form />;
+    </div>
+  );
 }
 
 export default App;
