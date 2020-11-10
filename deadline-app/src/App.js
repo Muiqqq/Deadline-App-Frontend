8700b1462efeb2d6b11fd21bfd2cebee603de04d
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-10 15:02:33 +0200

    Initial Todo component commit.

--- deadline-app/src/App.js
+++ deadline-app/src/App.js
@@ -3,6 +3,7 @@ import './scss/Main.scss';
 // Import components
 import Form from './components/Form';
 import TodoList from './components/Todolist';
+import Todo from './components/Todo';
 
 function App() {
   return (
