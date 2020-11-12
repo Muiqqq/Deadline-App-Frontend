9a01cb570a14564a07c1da1cd3c66074179fd6e8
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-11-12 11:16:16 +0200

    Add renderList(list) for MockListComponent, return it in render()

--- deadline-app/src/SortTestComponent.js
+++ deadline-app/src/SortTestComponent.js
@@ -36,6 +36,26 @@ class MockListComponent extends React.Component {
       ],
     };
   }
+
+  renderList = (list) => {
+    const arr = list.map((element) => {
+      return (
+        <li key={element.id}>
+          <h4>{element.isdone}</h4>
+          <h4>{element.priority}</h4>
+          <h4>{element.name}</h4>
+          <h4>{element.date}</h4>
+        </li>
+      );
+    });
+    return <ul>{arr}</ul>;
+  };
+
+  render() {
+    return (
+      <div className="mocklist">{this.renderList(this.state.tasklist)}</div>
+    );
+  }
 }
 
 export default MockListComponent;
