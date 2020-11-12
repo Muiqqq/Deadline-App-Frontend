e9c299e48b364201b6187d36e2e536bf922ad0c6
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-11-12 12:53:16 +0200

    Add SortButtonListComponent in SortComponent.js, initial implementation.

--- deadline-app/src/SortComponent.js
+++ deadline-app/src/SortComponent.js
@@ -1,1 +1,20 @@
 import React from "react";
+
+class SortButtonListComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <ul className="sortbuttonlist">
+        <li>TEST</li>
+        <li>TEST2</li>
+        <li>TEST3</li>
+      </ul>
+    );
+  }
+}
+
+export default SortButtonListComponent;
