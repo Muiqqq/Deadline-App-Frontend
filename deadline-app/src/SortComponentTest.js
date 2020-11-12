a781786c71ce3793a368ec429958da280f754e2a
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-11-12 10:49:37 +0200

    Add class component MockListComponent & state initialization for it

--- deadline-app/src/SortComponentTest.js
+++ deadline-app/src/SortComponentTest.js
@@ -1,1 +1,39 @@
 import React from "react";
+
+class MockListComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [
+        {
+          id: 1,
+          name: "B. Milk for the crocs.",
+          description: "-",
+          date: "YYYY-MM-DD",
+          priority: 2,
+          tasklistname: "TaskListA",
+          isdone: false,
+        },
+        {
+          id: 2,
+          name: "C. Milk for the cats.",
+          description: "-",
+          date: "YYYY-MM-DD",
+          priority: 3,
+          tasklistname: "TaskListA",
+          isdone: false,
+        },
+        {
+          id: 3,
+          name: "A. Milk for the hedgehogs.",
+          description: "-",
+          date: "YYYY-MM-DD",
+          priority: 1,
+          tasklistname: "TaskListA",
+          isdone: false,
+        },
+      ],
+    };
+  }
+}
