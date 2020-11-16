90d05782186c2206e2751a1f36a2fb0206b1b883
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-16 19:29:54 +0200

    Initial deleteHandler commit to delete button.

--- deadline-app/src/components/Todo.js
+++ deadline-app/src/components/Todo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Todo = ({ todo }) => {
+  const deleteHandler = () => {};
   return (
     <li className='todo-item'>
       <p>{todo[2]}</p>
       <p>{todo[1]}</p>
       <p>{todo[0]}</p>
-      <button>
+      <button onClick={deleteHandler}>
         <i class='fas fa-trash'></i>
       </button>
     </li>
