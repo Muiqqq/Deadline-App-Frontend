c569a2d4c38cba41dc61bf8240fff0c0f266bc67
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-16 10:55:44 +0200

    Change order of todo-item elements shown.
    Priority, date, name.

--- deadline-app/src/components/Todo.js
+++ deadline-app/src/components/Todo.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Todo = ({ todo }) => {
   return (
     <li className='todo-item'>
+      <p>{todo[2]}</p>
       <p>{todo[1]}</p>
       <p>{todo[0]}</p>
-      <p>{todo[2]}</p>
     </li>
   );
 };
