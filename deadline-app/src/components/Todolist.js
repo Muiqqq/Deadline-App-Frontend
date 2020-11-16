b4001a093785ebb15381ddf06cb218f981ccadc6
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-16 10:04:26 +0200

    Remove unnecessary comments from TodoList.js.

--- deadline-app/src/components/Todolist.js
+++ deadline-app/src/components/Todolist.js
@@ -2,13 +2,6 @@ import React from 'react';
 // Import components
 import Todo from './Todo';
 
-// const TodoList = ({ todos }) => {
-//   const todoItem = todos.map((todo, index) => {
-//     return <Todo key={index} todo={todo.name} />;
-//   });
-//   return <div>{todoItem}</div>;
-// };
-
 const TodoList = (props) => {
   const todoItems = props.todos.map((todo, index) => {
     return <Todo key={index} todo={todo.name} />;
