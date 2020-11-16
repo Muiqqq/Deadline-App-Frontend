e06c1b0800fb56b52a3d2438981393e6928fe2ec
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-16 10:17:39 +0200

    Add function toArray to Todolist.js.
    Extracts values from an object. Call toArray in
    TodoList when passing down Todo prop to Todo component.
    Thus Todo component gets to access all the values.

--- deadline-app/src/components/Todolist.js
+++ deadline-app/src/components/Todolist.js
@@ -2,9 +2,11 @@ import React from 'react';
 // Import components
 import Todo from './Todo';
 
+const toArray = (obj) => Object.values(obj);
+
 const TodoList = (props) => {
   const todoItems = props.todos.map((todo, index) => {
-    return <Todo key={index} todo={todo.name} />;
+    return <Todo key={index} todo={toArray(todo)} />;
   });
   return (
     <div className='list'>
