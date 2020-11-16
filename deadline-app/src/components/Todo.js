b4a054c71a8b06efe44fa63bbdc3c8cfe50b7464
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-11-16 10:53:01 +0200

    Add functionality to Todo component to show date and priority.

--- deadline-app/src/components/Todo.js
+++ deadline-app/src/components/Todo.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Todo = ({ todo }) => {
-  return <li className='todo-item'>{todo}</li>;
+  return (
+    <li className='todo-item'>
+      <p>{todo[1]}</p>
+      <p>{todo[0]}</p>
+      <p>{todo[2]}</p>
+    </li>
+  );
 };
 
 export default Todo;
