95fc22ae635c4e97e8c8e9a339b849de0da809fe
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-11-17 11:19:01 +0200

    Disable eslint complaint of a missing dependency in SortComponent.js.
    Adda comment which explains the whole thing.

--- deadline-app/src/components/SortComponent.js
+++ deadline-app/src/components/SortComponent.js
@@ -42,6 +42,14 @@ function SortButtonListComponent(props) {
       }
     });
     updateTasklist(tasklist);
+
+    // Below is necessary to stop eslint from complaining about
+    // a missing dependency for useEffect().
+    // The missing dependency is the tasklist being sorted, but
+    // having it in the dep array causes an infinite loop.
+    // Not having it there seems to work just fine (so far).
+
+    // eslint-disable-next-line
   }, [sortAscending, previouslyClicked, updateTasklist]);
 
   const handleClick = (buttonLabel) => {
