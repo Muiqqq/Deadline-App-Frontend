70d7b0803d2248fb9988ae3310b3937ec68aa8dd
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-11-30 13:21:55 +0200

    Add functionality to fetch todo data from API.
    Data is fetched once initially, otherwise everything works as previously.
    Need to implement api requests elsewhere as well.

--- deadline-app/src/components/ListComponent.js
+++ deadline-app/src/components/ListComponent.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from '../config/axiosconfig';
 // Import components
 import TodoList from './Todolist';
 
 const ListComponent = ({ todos, todoHandler, collapsibleStates }) => {
+  const [isLoaded, setIsLoaded] = useState(null);
+  const [lists, setLists] = useState([]);
+  const [tasks, setTasks] = useState([]);
   const list = [];
   todos.forEach((todo) => {
     if (!list.includes(todo.list)) {
@@ -10,10 +14,41 @@ const ListComponent = ({ todos, todoHandler, collapsibleStates }) => {
     }
   });
 
+  // Fetch lists
+  useEffect(() => {
+    axios.get('/lists').then((res) => {
+      if (res.status === 200) {
+        setLists(res.data);
+      } else {
+        // implement error handling if fetch failed
+      }
+    });
+  }, []);
+
+  // Fetch todos
+  useEffect(() => {
+    axios.get('/todos').then((res) => {
+      if (res.status === 200) {
+        setTasks(res.data);
+      } else {
+        // implement error handling if fetch failed
+      }
+    });
+  }, []);
+
+  // Send fetched todos and lists to parent
+  useEffect(() => {
+    if (lists.length > 0 && tasks.length > 0) {
+      setIsLoaded(true);
+      todoHandler.fetch(lists, tasks);
+    }
+  }, [todoHandler, lists, tasks]);
+
   const generateLists = list.map((listItem) => {
     const filtered = todos.filter((todo) => todo.list === listItem);
+    // Change key to listid once refactored
     return (
-      <ul>
+      <ul key={listItem}>
         <h3>{listItem}</h3>
         <TodoList
           todos={filtered}
@@ -24,7 +59,11 @@ const ListComponent = ({ todos, todoHandler, collapsibleStates }) => {
     );
   });
 
-  return <div className='list'>{generateLists}</div>;
+  if (!isLoaded) {
+    return <div className='list'>Fetching data...</div>;
+  } else {
+    return <div className='list'>{generateLists}</div>;
+  }
 };
 
 export default ListComponent;
