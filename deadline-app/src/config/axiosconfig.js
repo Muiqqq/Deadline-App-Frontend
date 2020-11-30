f23c15a0c43fb0fa1a6f933f6f6d642d334cfa7d
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-11-30 14:35:58 +0200

    Add heroku url as comment to axiosconfig.js

--- deadline-app/src/config/axiosconfig.js
+++ deadline-app/src/config/axiosconfig.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-
+// Heroku url:
+// https://tamk-4a00ez62-3001-group11.herokuapp.com/api
 const config = { baseURL: 'http://localhost:8080/api' };
 const instance = axios.create(config);
 
