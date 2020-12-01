fb961a994c5de25b9adbb6b40c1513db7409297d
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-12-01 11:40:55 +0200

    Change axios baseURL to our cloud url.

--- deadline-app/src/config/axiosconfig.js
+++ deadline-app/src/config/axiosconfig.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 // Heroku url:
 // https://tamk-4a00ez62-3001-group11.herokuapp.com/api
-const config = { baseURL: 'http://localhost:8080/api' };
+const config = {
+  baseURL: 'https://tamk-4a00ez62-3001-group11.herokuapp.com/api',
+};
 const instance = axios.create(config);
 
 export default instance;
